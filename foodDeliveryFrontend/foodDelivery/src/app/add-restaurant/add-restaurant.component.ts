727c7cd655972ee551566eebf40fb0460a319ffe
JCouz <couzensj@live.co.uk>
2025-01-31 15:48:28 +0000

    WIP Add restaurant

--- foodDeliveryFrontend/foodDelivery/src/app/add-restaurant/add-restaurant.component.ts
+++ foodDeliveryFrontend/foodDelivery/src/app/add-restaurant/add-restaurant.component.ts
@@ -1,12 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-restaurant',
   standalone: false,
-  
   templateUrl: './add-restaurant.component.html',
-  styleUrl: './add-restaurant.component.css'
+  styleUrl: './add-restaurant.component.css',
 })
 export class AddRestaurantComponent {
+  restaurantData = { name: '', location: '', rating: 0 };
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post('/restaurants', this.restaurantData).subscribe(
+      (response) => {
+        console.log('Restaurant added:', response);
+        this.restaurantData = { name: '', location: '', rating: 0 };
+      },
+      (error) => {
+        console.error('Error adding restaurant:', error);
+      }
+    );
+  }
 }
